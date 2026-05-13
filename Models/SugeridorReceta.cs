d88539b13762e75ee16f9f221c79ea76356ec704
PRISCILA DENISE NAIMAN <49551280@ort>
2026-05-13 08:35:19 -0300

    modelos hechos

---
+++ Models/SugeridorReceta.cs
@@ -0,0 +1,86 @@
+class SugeridorReceta
+{
+    public string nombre {get;set;}
+    public DateTime fechaNacimiento {get;set;}
+    public string tipoComida {get;set;}
+    public int presupuesto {get;set;}
+    public int cantComensales {get;set;}
+    
+    public int calcularEdad()
+    {
+        return (DateTime.Today - this.fechaNacimiento).Year;
+    }
+    public string DeterminarPlato()
+    {
+        if(this.tipoComida == "Caliente" && this.presupuesto < 3000)
+        {
+            return "Fideos con manteca";
+        }
+        else if (this.tipoComida == "Caliente" && this.presupuesto <= 7000 && this.presupuesto >= 3000)
+        {
+            return "Arroz con verduras salteadas";
+        }
+        else if (this.tipoComida == "Caliente" && this.presupuesto > 7000)
+        {
+            return "Pollo al horno con guarnición";
+        }
+        else if(this.tipoComida == "Frio" && this.presupuesto < 3000)
+        {
+            return "Ensalada simple";
+        }
+        else if (this.tipoComida == "Frio" && this.presupuesto <= 7000 && this.presupuesto >= 3000)
+        {
+            return "Ensalada completa con proteína";
+        }
+        else
+        {
+            return "Tabla de fiambres y quesos";
+        }
+    }
+    public int CalcularTiempo()
+    {
+        if(this.tipoComida == "Caliente" && (this.cantComensales == 1 || this.cantComensales == 2 || this.cantComensales == 3))
+        {
+            return 20;
+        }
+        else if (this.tipoComida == "Caliente" && this.cantComensales < 7 && this.cantComensales > 4)
+        {
+            return 40;
+        }
+        else if (this.tipoComida == "Caliente" && this.cantComensales > 8)
+        {
+            return 80;
+        }
+        else if(this.tipoComida == "Frio" && (this.cantComensales == 1 || this.cantComensales == 2 || this.cantComensales == 3))
+        {
+            return 10;
+        }
+        else if (this.tipoComida == "Frio" && this.cantComensales < 7 && this.cantComensales > 4)
+        {
+            return 20;
+        }
+        else
+        {
+            return 40;
+        }
+    }
+    public string DeterminarDificultad()
+    {
+        if (this.presupuesto < 3000  &&  (this.cantComensales == 1 || this.cantComensales == 2 || this.cantComensales == 3))
+        {
+            return "Principiante";
+        }
+        else if (this.presupuesto < 3000  &&  this.cantComensales <= 7 && this.cantComensales >= 4 || this.presupuesto >= 3000 && this.presupuesto <= 7000 && (this.cantComensales == 1 || this.cantComensales == 2 || this.cantComensales == 3) || this.presupuesto >= 3000 && this.presupuesto <= 7000 && this.cantComensales >= 4 || this.presupuesto > 7000 && this.cantComensales <= 7 && this.cantComensales >= 1)
+        {
+            return "Intermedio";
+        }
+        else if (this.presupuesto >= 7000 && cantComensales >= 8)
+        {
+            return "Avanzado";
+        }
+        else
+        {
+            return "No se pudo calcular nivel de dificultad"
+        }
+    }
+}
